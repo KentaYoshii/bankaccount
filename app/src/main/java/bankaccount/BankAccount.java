44e1c554a96ba7b64311d56342a3f0df41d66070
KentaYoshii <kenta_yoshii@brown.edu>
2022-11-11 15:41:40 -0500

    initial commit

---
+++ app/src/main/java/bankaccount/BankAccount.java
@@ -0,0 +1,17 @@
+
+
+public class BankAccount {
+    int curBalance;
+
+    public BankAccount(int initBalance){
+        this.curBalance = initBalance;
+    }
+
+    public void deposit(int k){
+
+    }
+
+    public void withdraw(int k){
+
+    }
+}
